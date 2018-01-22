b1be7a1503c5cce98e8df6bc9500afdb13a28d09
Aleksi Simell <aleksi.simell@teliacompany.com>
2018-01-22 15:38:49 +0200

    Added comments to clarify which urls are for which environment

--- salesforce_configs/metadata_configurations/replace_button_urls.py
+++ salesforce_configs/metadata_configurations/replace_button_urls.py
@@ -8,9 +8,9 @@ sys.path.append(PROJECT_ROOT)
 ########## REPLACE OPPORTUNITY BUTTON URL ENDPOINTS ###############
 oppo = "Opportunity.object"
 fileToUpdate = os.path.join(PROJECT_ROOT, "endpoints", "objects", oppo)
-textToSearch = "http://glock.stadi.sonera.fi"
-textToSearch2 = "http://sutil.stadi.sonera.fi"
-textToReplace = "http://kovalainen.stadi.sonera.fi"
+textToSearch = "http://glock.stadi.sonera.fi"   # prod
+textToSearch2 = "http://sutil.stadi.sonera.fi"  # preprod
+textToReplace = "http://kovalainen.stadi.sonera.fi" # dev/test
 
 
 f = open(fileToUpdate,'r')
@@ -29,9 +29,9 @@ f.close()
 ########## REPLACE ORDER BUTTON URL ENDPOINTS ###############
 order = "Order.object"
 fileToUpdate = os.path.join(PROJECT_ROOT, "endpoints", "objects", order)
-textToSearch = "http://glock.stadi.sonera.fi"
-textToSearch2 = "http://sutil.stadi.sonera.fi"
-textToReplace = "http://kovalainen.stadi.sonera.fi"
+textToSearch = "http://glock.stadi.sonera.fi"   # prod
+textToSearch2 = "http://sutil.stadi.sonera.fi"  # preprod
+textToReplace = "http://kovalainen.stadi.sonera.fi" # dev/test
 
 
 f = open(fileToUpdate,'r')
