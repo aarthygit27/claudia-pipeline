cab8704fa88b8c0a5c5a02d1c245a03e8be4708a
Aleksi Simell <aleksi.simell@teliacompany.com>
2018-02-07 14:22:59 +0200

    Revert "Added git commands in comments"
    This reverts commit ca1464b1ab66ace373cf1e9a65b1827b22c7ad25.

--- claudia_single_file_deployment/create_build_file2.py
+++ claudia_single_file_deployment/create_build_file2.py
@@ -16,12 +16,10 @@ buildfile.close()
 
 # Inserts cleared buildfile_deletesrc file and new exceptions from files_to_exclude.txt to temp list
 # Only lines starting with "src" is added, because there might be some irrelevant files also
-# git diff --name-only HEAD~1 > files_to_exclude.txt
 exclude_file = open('files_to_exclude.txt', 'r')
 exclude_set = map(lambda x: x.rstrip(), exclude_file.readlines())
 exclude_set.insert(1, 'src/package.xml')
 print exclude_set
-# git ls-files > files_to_include.txt
 include_file = open('files_to_include.txt', 'r')
 buildfile = open("buildfile_deletesrc.xml", 'r')
 temp = buildfile.readlines()
