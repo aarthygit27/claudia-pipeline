c8786f814b8ccc8faffa26588841f1f9fc145cee
Aleksi Simell <aleksi.simell@teliacompany.com>
2018-03-05 14:41:57 +0200

    Slight refactoring. Still untested

--- salesforce_configs/metadata_configurations/replace_endpoints.py
+++ salesforce_configs/metadata_configurations/replace_endpoints.py
@@ -7,13 +7,19 @@ sys.path.append(PROJECT_ROOT)
 
 ET.register_namespace("", "http://soap.sforce.com/2006/04/metadata")
 
+def find_url(tree, element_type):
+    return tree.find("{http://soap.sforce.com/2006/04/metadata}" + element_type)
+
+
 rs_location = "endpoint_for_contact_outbound_message.remoteSite"
 endpoint_for_contact_outbound_message_remotesite = "https://emily.extra.sonera.fi:62100"
+element_type = "url"
 
 path = os.path.join(PROJECT_ROOT, "endpoints", "remoteSiteSettings", rs_location)
 parsed_file = ET.parse(path)
 root = parsed_file.getroot()
-a = root.find("{http://soap.sforce.com/2006/04/metadata}" + "url")
+# a = root.find("{http://soap.sforce.com/2006/04/metadata}" + "url")
+a = find_url(root, element_type)
 a.text = endpoint_for_contact_outbound_message_remotesite
 print "\nChanged " + rs_location + " <url>" + endpoint_for_contact_outbound_message_remotesite + "</url>"
 parsed_file.write(path)
@@ -21,12 +27,13 @@ parsed_file.write(path)
 
 rs_location = "GESB_AC.remoteSite"
 endpoint_for_gesb_ac_remotesite = "https://emily.extra.sonera.fi:62503"
-element_type = "url"
+# element_type = "url"
 
 path = os.path.join(PROJECT_ROOT, "endpoints", "remoteSiteSettings", rs_location)
 parsed_file = ET.parse(path)
 root = parsed_file.getroot()
-a = root.find("{http://soap.sforce.com/2006/04/metadata}" + element_type)
+# a = root.find("{http://soap.sforce.com/2006/04/metadata}" + element_type)
+a = find_url(root, element_type)
 a.text = endpoint_for_gesb_ac_remotesite
 print "\nChanged " + rs_location + " <" + element_type + ">" + endpoint_for_gesb_ac_remotesite + "</"+ element_type + ">"
 parsed_file.write(path)
@@ -34,12 +41,13 @@ parsed_file.write(path)
 
 rs_location = "GESB_AV.remoteSite"
 endpoint_for_gesb_av_remotesite = "https://emily.extra.sonera.fi:62502"
-element_type = "url"
+# element_type = "url"
 
 path = os.path.join(PROJECT_ROOT, "endpoints", "remoteSiteSettings", rs_location)
 parsed_file = ET.parse(path)
 root = parsed_file.getroot()
-a = root.find("{http://soap.sforce.com/2006/04/metadata}" + element_type)
+# a = root.find("{http://soap.sforce.com/2006/04/metadata}" + element_type)
+a = find_url(root, element_type)
 a.text = endpoint_for_gesb_av_remotesite
 print "\nChanged " + rs_location + " <" + element_type + ">" + endpoint_for_gesb_av_remotesite + "</"+ element_type + ">"
 parsed_file.write(path)
@@ -52,7 +60,8 @@ element_type = "endpointUrl"
 path = os.path.join(PROJECT_ROOT, "endpoints", "workflows", rs_location)
 parsed_file = ET.parse(path)
 root = parsed_file.getroot()
-a = root.find("{http://soap.sforce.com/2006/04/metadata}outboundMessages").find('{http://soap.sforce.com/2006/04/metadata}endpointUrl')
+# a = root.find("{http://soap.sforce.com/2006/04/metadata}outboundMessages").find('{http://soap.sforce.com/2006/04/metadata}endpointUrl')
+a = find_url(find_url(root, "outboundMessages"), element_type)
 a.text = contact_workflow
 print "\nChanged " + rs_location + " <" + element_type + ">" + contact_workflow + "</"+ element_type + ">"
 parsed_file.write(path)
