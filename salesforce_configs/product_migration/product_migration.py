1b087eec98e2c2dce346fc65fa53165fe226476f
Aleksi Simell <aleksi.simell@teliacompany.com>
2018-04-18 16:05:04 +0300

    Reduce load in main(). Write hashed products file implemented

--- salesforce_configs/product_migration/product_migration.py
+++ salesforce_configs/product_migration/product_migration.py
@@ -49,10 +49,10 @@ def handle(filename, products, i, j=-1, source_dir=SOURCE_DIR, dest_dir=NEW_PROD
 
 def write_files_from_products(filename, source_dir=SOURCE_DIR, dest_dir=NEW_PRODUCTS_DEST_DIR):
     solution_areas = get_ids(filename, 32) # TELIA_SUB_SOLUTION_AREA__C
-    handle("mastersolutionarea", solution_areas, 0)
+    handle("mastersolutionarea", solution_areas, 0, source_dir=source_dir, dest_dir=dest_dir)
 
     offerings = get_ids(filename, 33)  # TELIA_CONTRACT_OFFERING_ID__C
-    handle("offerings", offerings, 9)   # OFFERING_ID__C
+    handle("offerings", offerings, 9, source_dir=source_dir, dest_dir=dest_dir)   # OFFERING_ID__C
 
     products = get_ids(filename)
     handle("attributeassignments", products, 48, source_dir=source_dir, dest_dir=dest_dir)    # VLOCITY_CMT__OBJECTID__C
@@ -61,31 +61,47 @@ def write_files_from_products(filename, source_dir=SOURCE_DIR, dest_dir=NEW_PROD
     handle("productrelationship", products, 20, 22, source_dir=source_dir, dest_dir=dest_dir)     # VLOCITY_CMT__PRODUCT2ID__C, VLOCITY_CMT__RELATEDPRODUCTID__C
     handle("pricebookentry", products, 3, source_dir=source_dir, dest_dir=dest_dir)   # PRODUCT2ID
 
-
-def write_files_from_pricebook_entries(source_dir=SOURCE_DIR, dest_dir=NEW_PRODUCTS_DEST_DIR):
     pricebooks = get_ids(os.path.join(dest_dir, "pricebookentry2"), 2) # PRICEBOOK2ID
     handle("pricebook", pricebooks, 0, source_dir=source_dir, dest_dir=dest_dir)
 
-
-def write_files_from_attribute_assignments(source_dir=SOURCE_DIR, dest_dir=NEW_PRODUCTS_DEST_DIR):
     attribute_categories = get_ids(os.path.join(dest_dir, "attributeassignments2"), 12) # VLOCITY_CMT__ATTRIBUTECATEGORYID__C
     handle("vlocityattributecategory", attribute_categories, 0, source_dir=source_dir, dest_dir=dest_dir)
 
     attributes = get_ids(os.path.join(dest_dir, "attributeassignments2"), 19) # VLOCITY_CMT__ATTRIBUTEID__C
     handle("vlocityattribute", attributes, 0, source_dir=source_dir, dest_dir=dest_dir)
 
-
-def write_files_from_product_configuration_procedures(source_dir=SOURCE_DIR, dest_dir=NEW_PRODUCTS_DEST_DIR):
     entity_filters = get_ids(os.path.join(dest_dir, "productconfigurationprocedures2"), 12) # VLOCITY_CMT__ENTITYFILTERID__C
     handle("vlocityentityfilter", entity_filters, 0, source_dir=source_dir, dest_dir=dest_dir)
     handle("vlocityentityfiltercondition", entity_filters, 9, source_dir=source_dir, dest_dir=dest_dir)   # VLOCITY_CMT__ENTITYFILTERID__C
     handle("vlocityrulefilter", entity_filters, 9, source_dir=source_dir, dest_dir=dest_dir)   # VLOCITY_CMT__ENTITYFILTERID__C
 
-
-def write_files_from_vlocity_rule_filters(source_dir=SOURCE_DIR, dest_dir=NEW_PRODUCTS_DEST_DIR):
     vlocity_rules = get_ids(os.path.join(dest_dir, "vlocityrulefilter2"), 8) # VLOCITY_CMT__RULEID__C
     handle("vlocityrule", vlocity_rules, 0, source_dir=source_dir, dest_dir=dest_dir)
     handle("vlocityruleaction", vlocity_rules, 8, source_dir=source_dir, dest_dir=dest_dir)   # VLOCITY_CMT__RULEID__C
+
+
+def write_hashed_products_file(source_products, target_products):
+    source = []
+    with open(source_products + ".csv", "rb") as f:
+        reader = csv.reader(f, delimiter=",", quotechar="\"")
+        header = reader.next()   # skip the header
+        for row in reader:
+            source.append(row)
+    target = []
+    with open(target_products + ".csv", "rb") as f:
+        reader = csv.reader(f, delimiter=",", quotechar="\"")
+        reader.next()   # skip the header
+        for row in reader:
+            target.append(row)
+
+    for product in source:
+        h = product[56]    # PRODUCT_HASH__C
+        for product2 in target:
+            if h == product2[56]:   # PRODUCT_HASH__C
+                product[0] = product2[0]    # Replace source Ids with target Ids
+                break
+    write_data("products_hashed.csv", header, source, ".")
+
 
 
 ###################################################################################################
@@ -93,12 +109,14 @@ def write_files_from_vlocity_rule_filters(source_dir=SOURCE_DIR, dest_dir=NEW_PR
 ###################################################################################################
 
 def main():
+    # NEW PRODUCTS
     write_files_from_products("products_new")
-    # Rest of the files are written from files written by either products steps or later steps so filenames are not required
-    write_files_from_pricebook_entries()
-    write_files_from_attribute_assignments()
-    write_files_from_product_configuration_procedures()
-    
+
+    # EXISTING PRODUCTS
+    write_files_from_products("products_existing", dest_dir=EXISTING_PRODUCTS_DEST_DIR)
+
+    write_hashed_products_file("products_existing", "products_preprod")
+
 
 if __name__ == '__main__':
     main()
